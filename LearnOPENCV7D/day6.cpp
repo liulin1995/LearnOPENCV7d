5bec8070e280aeac6b54b40be642ce8893c61b30
liulin1995 <840395949@qq.com>
2017-11-29 20:24:59 +0800

    Good Day!

--- LearnOPENCV7D/day6.cpp
+++ LearnOPENCV7D/day6.cpp
@@ -1,33 +1,45 @@
 #include<iostream>
 #include<opencv2/highgui.hpp>
 #include<opencv2/imgproc.hpp>
-#include<opencv2/ml.hpp>
 #include"opencv2/objdetect.hpp"
+#include<opencv2/ml.hpp>
+
 
 using namespace cv::ml;
 using namespace cv;
 using namespace std;
 
 int main() {
-
+	//the param should be noticed!!! 
+	// I wasted 10  hours because it!
 	HOGDescriptor hogDefault(
 		Size(64, 128),
 		Size(16, 16),
 		Size(8, 8),
 		Size(8, 8),
-		9, 0, -1.0, 0.2, 1, 64, 0
+		9, 0, -1, 0,0.2, 1, 64, 0
 	);
+	
 	vector<float> svmDetectorDefault = hogDefault.getDefaultPeopleDetector();
 	hogDefault.setSVMDetector(svmDetectorDefault);
+	
+	float finalHeight = 800.0;
 
-	float finalHeight = 800;
-	string imagePath = "boy.jpg";
-	Mat im = imread(imagePath,0);
+	String imagePath = "race.jpg";
+	Mat im = imread(imagePath, IMREAD_COLOR);
+
+	if (im.empty())
+	{
+		cout << "Open Image error!\n";
+		return 1;
+	}
+	float finalWidth = (finalHeight  *  im.cols) / im.rows;
 	
-	float finalWidth = (finalHeight * im.cols) / im.rows;
-	
-	resize(im, im, Size(finalHeight, finalWidth), INTER_AREA);
-	
+	resize(im, im, Size(finalWidth, finalHeight));
+
+
+
+
 	vector<Rect> bboxes;
 	vector<double> weights;
 
@@ -42,7 +54,7 @@ int main() {
 	
 	if (!bboxes.empty())
 	{
-		cout << "Default Detector ::¡¡£ð£å£ä£å£ó£ô£ò£é£á£î£ó¡¡£ä£å£ô£å£ã£ô£å£ä£º" << bboxes.size() << endl;
+		cout << "Default Detector :: pedestrians detected:" << bboxes.size() << endl;
 		vector<Rect>::const_iterator loc = bboxes.begin();
 		vector<Rect>::const_iterator end = bboxes.end();
 		for (; loc != end; ++loc)
